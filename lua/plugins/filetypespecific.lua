9cd0fa265d3fe5da4e549051286e8a32877de0ea
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2026-07-01 11:27:56 +0900

    feat(plugin): add "MeanderingProgrammer/render-markdown.nvim"

--- lua/plugins/filetypespecific.lua
+++ lua/plugins/filetypespecific.lua
@@ -40,6 +40,13 @@ return {
         },
     },
     {
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
+    {
         "iamcco/markdown-preview.nvim",
         ft = { "markdown", "pandoc.markdown", "rmd" },
         build = 'sh -c "cd app & yarn install"',
